bdaf3d45ab2ef0b60ed15f778ae9885c4d03711f
Giacomo <giaccneto@hotmail.com>
2024-11-14 07:36:28 -0400

    GameMinProjection

---
+++ dslist/src/main/java/com/giaccneto/dslist/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.giaccneto.dslist.projections;
+
+public interface GameMinProjection {
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getimgUrl();
+    String getShortDescription();
+    Integer getPositon();
+
+
+}
